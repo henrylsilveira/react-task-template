8272907878deda6ee43cb5e09df6d73a9f2576c2
henrylsilveira <henrylsilveira@gmail.com>
2021-03-21 11:10:23 -0300

    21/03/2021

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
-
-import '../styles/tasklist.scss'
-
 import { FiTrash, FiCheckSquare } from 'react-icons/fi'
+import '../styles/tasklist.scss'
 
 interface Task {
   id: number;
@@ -15,15 +13,29 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    newTaskTitle.trim() !== '' &&
+      setTasks([...tasks, {
+        id: Math.random(),
+        title: newTaskTitle.trim(),
+        isComplete: false
+      }])
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map((task) => {
+      return task.id === id ? {
+        ...task,
+        isComplete: !task.isComplete
+      } : task
+    })
+
+    setTasks(newTasks)
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const newTasks = tasks.filter((task) => task.id !== id)
+
+    setTasks(newTasks)
   }
 
   return (
@@ -72,3 +84,66 @@ export function TaskList() {
     </section>
   )
 }
+/*
+## O que devo editar na aplicação?
+
+Com o template já clonado, as depêndencias instaladas, 
+você deve completar onde não possui código com o código para atingir os objetivos de cada teste. Nesse desafio, você deve editar apenas o seguinte arquivo para completar as funcionalidades da aplicação:
+
+- [src/components/TaskList.tsx;]
+(https://github.com/rocketseat-education/ignite-template-reactjs-conceitos-do-react/blob/main/src/components/TaskList.tsx)
+
+### components/TaskList.tsx
+
+Esse é o componente responsável por todas as funcionalidades da aplicação, 
+é um componente simples, mas onde botaremos em prática várias partes da manipulação do estado.
+
+Você deve criar as funcionalidades para as três funções presentes nesse arquivo, que são:
+
+- **handleCreateNewTask**: Deve ser possível adicionar uma nova task no estado de `tasks`, 
+com os campos `id` que deve ser gerado de forma aleatória, `title` que deve ser um texto e `isComplete` 
+que deve iniciar como false.
+- **handleToggleTaskCompletion:** Deve alterar o status de `isComplete` para uma task com um ID 
+específico que é recebido por parâmetro.
+- **handleRemoveTask:** Deve receber um ID por parâmetro e remover a task que contém esse ID do estado.
+
+## Especificação dos testes
+
+Em cada teste, tem uma breve descrição no que sua aplicação deve cumprir para que o teste passe.
+
+Caso você tenha dúvidas quanto ao que são os testes, e como interpretá-los, dê uma olhada em **
+[nosso FAQ](https://www.notion.so/FAQ-Desafios-ddd8fcdf2339436a816a0d9e45767664)**
+
+Para esse desafio, temos os seguintes testes:
+
+### Teste TaskList.spec.tsx
+
+- **should be able to add a task**
+
+Para que esse teste passe, você deve permitir que task seja criada e com isso, exibida em tela. 
+As taks criadas devem conter os atributos seguindo o padrão da interface, que é:
+
+```tsx
+interface Task {
+  id: number;
+  title: string;
+  isComplete: boolean;
+}
+```
+
+- **should not be able to add a task with an empty title**
+
+Para que esse teste passe, antes de criar uma nova task, você deve validar se algo foi digitado no 
+input e não permitir a criação da task caso o valor seja vazio, caso o valor digitado seja vazio, você deve impedir a criação da task.
+
+- **should be able to remove a task**
+
+Para que esse teste passe, você deve permitir que ao clicar no botão com ícone de uma lixeira, a 
+task relacionada a esse botão seja removida do estado da aplicação, consequentemente sendo removida da tela.
+
+- **should be able to check a task**
+
+Para que esse teste passe, você deve permitir que ao clicar no checkbox ao lado da task, ela seja 
+marcada como concluída ou não concluída de acordo com seu estado atual, alterando seu valor de `isComplete` 
+de `false` para `true` ou ao contrário, de `true` para `false`.
+*/
